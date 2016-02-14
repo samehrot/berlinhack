ae9add8f1b21859445ddd85a8119649aabf17df9
samehrot <samehrot@cisco.com>
2016-02-14 05:55:28 +0530

    Adding Lib

--- src/main/java/org/verbot/DemoApplication.java
+++ src/main/java/org/verbot/DemoApplication.java
@@ -7,7 +7,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+/**
+ * 
+ * @author GUR40174
+ *
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = { "org.verbot" })
 @EnableAsync
